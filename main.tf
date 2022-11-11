92ee8b6e21ec2b79643997f21deed2b71eaa54f3
Raghu K <raghuk.vit@gmail.com>
2022-11-11 07:26:40 +0530

    Update tf-module-rds/

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "aws_rds_cluster" "default" {
+  depends_on                      = [aws_rds_cluster_parameter_group.default, aws_db_subnet_group.default]
+  for_each                        = var.rds
+  cluster_identifier              = "${var.env}-${each.key}-roboshop-rds"
+  engine                          = each.value.engine
+  engine_version                  = each.value.engine_version
+  database_name                   = "mydb"
+  master_username                 = "foo"
+  master_password                 = "bardfvdflvndfsk"
+  skip_final_snapshot             = true
+  db_cluster_parameter_group_name = "${var.env}-${each.key}-roboshop-rds"
+  db_subnet_group_name            = "${var.env}-${each.key}-roboshop-rds"
+}
+
+resource "aws_rds_cluster_parameter_group" "default" {
+  for_each    = var.rds
+  name        = "${var.env}-${each.key}-roboshop-rds"
+  family      = "aurora5.7"
+  description = "RDS default cluster parameter group"
+}
+
+resource "aws_db_subnet_group" "default" {
+  for_each   = var.rds
+  name       = "${var.env}-${each.key}-roboshop-rds"
+  subnet_ids = var.subnets
+
+  tags = {
+    Name = "${var.env}-${each.key}-roboshop-rds"
+  }
+}
+
