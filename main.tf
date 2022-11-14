33418808bf8c5154e0984aa4460f8a0d63a9a74a
Raghu K <raghuk.vit@gmail.com>
2022-11-14 06:52:59 +0530

    Update tf-module-rds/

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_rds_cluster" "default" {
-  depends_on                      = [aws_rds_cluster_parameter_group.default, aws_db_subnet_group.default]
-  cluster_identifier              = "${var.env}-${var.name}-roboshop-rds"
-  engine                          = var.engine
-  engine_version                  = var.engine_version
-  database_name                   = "mydb"
-  master_username                 = "foo"
-  master_password                 = "bardfvdflvndfsk"
-  skip_final_snapshot             = true
-  db_cluster_parameter_group_name = "${var.env}-${var.name}-roboshop-rds"
-  db_subnet_group_name            = "${var.env}-${var.name}-roboshop-rds"
+  depends_on          = [aws_rds_cluster_parameter_group.default, aws_db_subnet_group.default]
+  cluster_identifier  = "${var.env}-${var.name}-roboshop-rds"
+  engine              = var.engine
+  engine_version      = var.engine_version
+  database_name       = "mydb"
+  master_username     = "foo"
+  master_password     = "bardfvdflvndfsk"
+  skip_final_snapshot = true
+  //db_cluster_parameter_group_name = "${var.env}-${var.name}-roboshop-rds"
+  db_subnet_group_name = "${var.env}-${var.name}-roboshop-rds"
 }
 
 resource "aws_rds_cluster_parameter_group" "default" {
@@ -17,12 +17,12 @@ resource "aws_rds_cluster_parameter_group" "default" {
   description = "RDS default cluster parameter group"
 }
 
-resource "aws_db_subnet_group" "default" {
-  name       = "${var.env}-${var.name}-roboshop-rds"
-  subnet_ids = var.subnets
-
-  tags = {
-    Name = "${var.env}-${var.name}-roboshop-rds"
-  }
-}
+//resource "aws_db_subnet_group" "default" {
+//  name       = "${var.env}-${var.name}-roboshop-rds"
+//  subnet_ids = var.subnets
+//
+//  tags = {
+//    Name = "${var.env}-${var.name}-roboshop-rds"
+//  }
+//}
 
