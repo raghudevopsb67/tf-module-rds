ab5275279c2a877833a69db07fa9d05273474c0e
Raghu K <raghuk.vit@gmail.com>
2022-11-21 06:21:23 +0530

    Update tf-module-rds/

--- schema.tf
+++ schema.tf
@@ -1,14 +1,13 @@
-//resource "null_resource" "load-schema" {
-//  depends_on = [aws_rds_cluster.default]
-//
-//  provisioner "local-exec" {
-//    command = <<EOF
-//curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-//cd /tmp
-//unzip -o mysql.zip
-//cd mysql-main
-//
-//EOF
-//  }
-//
-//}
+resource "null_resource" "load-schema" {
+  depends_on = [aws_rds_cluster.default]
+
+  provisioner "local-exec" {
+    command = <<EOF
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -h ${aws_rds_cluster.default.endpoint} -u${local.RDS_USER} -p${local.RDS_PASS} <shipping.sql
+EOF
+  }
+}
